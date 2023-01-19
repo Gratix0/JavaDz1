db116b89f731cbddbfc9bd48f955f4959516859e
Gratix0 <oganisyan.sanya1@mail.ru>
2023-01-19 19:00:58 +0300

    1

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -1,37 +1,66 @@
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        int wonnaexit;
+        int divisionby0 = 0;
         double num1;
         double num2;
-        double ans = 0.0;
+        double ans = 1.0;
         char op;
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter two numbers: ");
-        num1 = reader.nextDouble();
-        num2 = reader.nextDouble();
-        System.out.print("\nEnter an operator (+, -, *, /, ^, !): ");
-        op = reader.next().charAt(0);
-        switch(op) {
-            case '+': ans = num1 + num2;
+        while (true) {
+
+            try {
+            Scanner reader = new Scanner(System.in);
+            System.out.print("If you wont next press any key. If you wont exit enter 0: ");
+            wonnaexit = reader.nextInt();
+            if (wonnaexit == 0) {
                 break;
-            case '-': ans = num1 - num2;
-                break;
-            case '*': ans = num1 * num2;
-                break;
-            case '/': ans = num1 / num2;
-                break;
-            case '^': ans = Math.pow(num1, num2);
-                break;
-            case  '!':
-                if (num1 == 0)
-                    ans = 1;
-                for (double i = 1.0; i < num2; i++)
-                    ans = ans*i;
-                break;
-            default:  System.out.printf("Error! Enter correct operator");
-                return;
+            }
+                System.out.print("Enter two numbers: ");
+                num1 = reader.nextDouble();
+                num2 = reader.nextDouble();
+                System.out.print("\nEnter an operator (+, -, *, /, ^, !): ");
+                op = reader.next().charAt(0);
+                switch (op) {
+                    case '+':
+                        ans = num1 + num2;
+                        break;
+                    case '-':
+                        ans = num1 - num2;
+                        break;
+                    case '*':
+                        ans = num1 * num2;
+                        break;
+                    case '/':
+                        if (num1 == 0)
+                            divisionby0 = 1;
+                        if (num2 == 0)
+                            divisionby0 = 1;
+                        ans = num1 / num2;
+                        break;
+                    case '^':
+                        ans = Math.pow(num1, num2);
+                        break;
+                    case '!':
+                        for (double i = 1.0; i <= num1; i++)
+                            ans = ans * i;
+                        break;
+                    default:
+                        System.out.printf("Error! Enter correct operator");
+                        return;
+                }
+
+        if (divisionby0 == 1) {
+            System.out.print("You division by zero\n");
+            break;
         }
         System.out.print("\nThe result is given as follows:\n");
-        System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+        System.out.print(ans + "\n");
+        }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
